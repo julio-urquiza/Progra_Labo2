88a0dbd709a3800a30c765a505f424231f223331
julio-urquiza <julio.cesar.urquiza4@gmail.com>
2022-09-05 20:27:30 -0300

    A

---
+++ 04.Sobrecarga/Ejercicio C02 - La estantería/Biblioteca/Estante.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Biblioteca
+{
+    public class Estante
+    {
+        private Producto[] productos;
+        private int ubicacionEstante;
+
+        private Estante(int capacidad)
+        {
+            this.productos = new Producto[capacidad];
+        }
+        public Estante(int capacidad,int ubicacion):this(capacidad)
+        {
+            this.ubicacionEstante=ubicacion;
+        }
+        public Producto[] GetProductos()
+        {
+            return this.productos;
+        }
+        public static string MostrarEstante(Estante e)
+        {
+            StringBuilder retorno=new StringBuilder();
+            retorno.AppendFormat("Ubicaion de estante: {0}\n", e.ubicacionEstante);
+            foreach(Producto p in e.productos)
+            {
+                if(p is not null)
+                {
+                    retorno.AppendLine(Producto.MostrarProducto(p));
+                }
+            }
+            return retorno.ToString();
+        }
+        public static bool operator ==(Estante e, Producto p)
+        {
+            //e.GetProductos().Contains(p);
+            foreach(Producto producto in e.productos)
+            {
+                if(producto==p)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public static bool operator !=(Estante e,Producto p)
+        {
+            return !(e==p);
+        }
+        public static bool operator +(Estante e,Producto p)
+        {
+            if(e!=p)
+            {
+                for (int i = 0; i < e.productos.Length; i++)
+                {
+                    if (e.productos[i] is null)
+                    {
+                        e.productos[i] = p;
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+        public static Estante operator -(Estante e,Producto p)
+        {
+            for(int i=0;i < e.productos.Length;i++)
+            {
+                if(e.productos[i]==p)
+                {
+                    e.productos[i]= null;
+                    break;
+                }
+            }
+            return e;
+        }
+
+    }
+}
---
+++ 04.Sobrecarga/Ejercicio C02 - La estantería/Biblioteca/Producto.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Biblioteca
+{
+    public class Producto
+    {
+        private string codigoDeBarra;
+        private string marca;
+        private float precio;
+
+        public Producto(string marca, string codigoDeBarra, float precio)
+        {
+            this.marca = marca;
+            this.codigoDeBarra = codigoDeBarra;
+            this.precio = precio;
+        }
+
+        public static explicit operator string(Producto p)
+        {
+            return p.codigoDeBarra;
+        }
+        public string GetMarca()
+        {
+            return this.marca;
+        }
+        public float GetPrecio()
+        {
+            return this.precio;
+        }
+        public static string MostrarProducto(Producto p)
+        {
+            return String.Format("Marca :{0}, Precio:{1}, codigo de barras_{2} ", p.marca, p.precio, p.codigoDeBarra);
+        }
+        public static bool operator ==(Producto p1,string marca)
+        {
+            return p1.marca == marca;
+        }
+        public static bool operator !=(Producto p1, string marca)
+        {
+            return !(p1==marca);
+        }
+        public static bool operator ==(Producto p1,Producto p2)
+        {
+            if (p1 is not null && p2 is not null)
+            {
+                return p1.marca == p2.marca && p1.codigoDeBarra==p2.codigoDeBarra;
+            }
+            return false;
+        }
+        public static bool operator !=(Producto p1, Producto p2)
+        {
+            return !(p1==p2);
+        }
+    }
+}
