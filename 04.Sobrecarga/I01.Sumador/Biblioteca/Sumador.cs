6468ec7f960578541490af5fd6a6b539690fc1bb
julio-urquiza <julio.cesar.urquiza4@gmail.com>
2022-09-05 21:44:20 -0300

    05/09/22 21.44

---
+++ 04.Sobrecarga/I01.Sumador/Biblioteca/Sumador.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Biblioteca
+{
+    public class Sumador
+    {
+        private int cantidadSumas;
+
+        public Sumador(int cantidadSumas)
+        {
+            this.cantidadSumas = cantidadSumas;
+        }
+        public Sumador() : this(0) { }
+
+        public static implicit operator int (Sumador s)
+        {
+            return s.cantidadSumas;
+        }
+        public static bool operator |(Sumador s1,Sumador s2)
+        {
+            return s1.cantidadSumas==s2.cantidadSumas;
+        }
+        public static long operator +(Sumador s1,Sumador s2)
+        {
+            return s1.cantidadSumas+s2.cantidadSumas;
+        }
+        public long Sumar(long a,long b)
+        {
+            this.cantidadSumas++;
+            return a + b;
+        }
+        public string Sumar(string a,string b)
+        {
+            this.cantidadSumas++;
+            return a + b;
+        }
+    }
+}
---
+++ 04.Sobrecarga/I01.Sumador/Consola/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Biblioteca;
+namespace Consola
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Sumador sumador1= new Sumador();
+            Sumador sumador2= new Sumador(2);
+
+            long cantidadSumas = sumador2;
+
+            if(sumador1|sumador2)
+            {
+                Console.WriteLine("diferente cantidad de sumas");
+            }
+            else
+            {
+                Console.WriteLine("igual cantidad de sumas");
+            }
+
+            Console.WriteLine(sumador1+sumador2);
+
+            Console.WriteLine(sumador1.Sumar(15,65));
+            Console.WriteLine(sumador2.Sumar("papa","pipi"));
+
+            Console.ReadKey();
+        }
+    }
+}
